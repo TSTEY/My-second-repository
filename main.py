d6c28dd0846b821a8ced6516cee9019c34ae47ff
TSTEY <tstey88gmail.com>
2024-05-04 19:21:46 +0500

    www

---
+++ main.py
@@ -0,0 +1,24 @@
+1
+# tst = 5
+# if tst > 5:
+#     print("true")
+# else:
+#     print("false")
+2
+# tst = 15
+# if tst > 10 & tst < 20:
+#     print("true")
+# else:
+#     print("false")
+3
+# tst = -5
+# if tst > 10 & tst < 0 or tst > -8 & tst < 30:
+#     print("true")
+# else:
+#     print("false")
+4
+# tst = ['a,b,c,d,e']
+# if len(tst) < 6 & len(tst) > 0:
+#     print("true")
+# else:
+#     print("false")
